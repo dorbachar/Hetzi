c8a991319f336ba521e277762b2cc52b59c24cc5
dorbachar <dor.bachar.1992@gmail.com>
2019-04-12 15:04:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hetzi_beta/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.hetzi_beta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mProductsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickProducts(View view) {
+        Intent intent = new Intent(this, EditProductsActivity.class);
+        startActivity(intent);
+    }
+}
